55a92480c090cad1926635e6333838c63c5d95e6
andre <andre@172.21.152.81>
2019-11-11 22:12:35 -0200

    Quinto Commit: Extract Method

--- src/modelo/Rental.java
+++ src/modelo/Rental.java
@@ -46,4 +46,11 @@ class Rental {
         }
         return thisAmount;
     }
+    
+    int getFrequentRenterPoints() {
+       if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+          return 2;
+       else
+          return 1;
+   }
 }
